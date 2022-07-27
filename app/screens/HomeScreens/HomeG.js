4b908eafe05053e5b5effe578e1ace9c37eb0ad6
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2022-07-27 17:32:00 -0500

    HOC typeSyntyax

--- app/screens/HomeScreens/HomeG.js
+++ app/screens/HomeScreens/HomeG.js
@@ -1,20 +1,30 @@
 import * as SecureStore from "expo-secure-store";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+// if (typeAccount == "vendedor") {
+// Vamos a mandar a buscar si existe una tienda con el uid del usuario
+// Si no hay, entonces podemos rendereizar un modal, o una pantalla con options={{modal}}
+//    -- Adentro de esto podemos poner una condiciopon si nos devuelve algo
+//      -- Si nos devuelve un documento, entonces podemos guardar este objeto en el "asyncStorage"
+//      -- Para luego utilizar los datos en la parte de "MyStore"
+//      __ Si no existe, entonces podemos navegar a la pantalla "CreateStore" que va  tener el tipo modal
+//}
 
-import React from "react";
-import { View, Text, Button } from "react-native";
-
-export default class HomeG extends React.Component {
-  // Checar como poner atrubutos en las clases
-  componentDidMount() {
+export default function HomeG() {
+  const [first, setfirst] = useState(null);
+  useEffect(() => {
     SecureStore.getItemAsync("uid").then((token) => {
       console.log(token);
+      setfirst(token);
     });
-  }
-  render() {
-    return (
-      <View>
-        <Text>HomeG -- Orders</Text>
-      </View>
-    );
-  }
+    return () => {
+      first;
+    };
+  }, []);
+
+  return (
+    <View>
+      <Text>{first}</Text>
+    </View>
+  );
 }
