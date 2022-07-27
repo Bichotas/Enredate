282df0ca289e99217ca2fcfb34b14e80f7d9fddd
CsarChvz <cesarchavezrodriguez9@gmail.com>
2022-07-26 22:38:18 -0500

    De esta forma si

--- app/screens/HomeScreens/HomeG.js
+++ app/screens/HomeScreens/HomeG.js
@@ -1,14 +1,18 @@
 import * as SecureStore from "expo-secure-store";
 
 import React from "react";
-import { View, Text } from "react-native";
-import { getUserUid } from "../../utils/session.client";
+import { View, Text, Button } from "react-native";
 
 export default class HomeG extends React.Component {
+  componentDidMount() {
+    SecureStore.getItemAsync("uid").then((token) => {
+      console.log(token);
+    });
+  }
   render() {
     return (
       <View>
-        <Text>Home</Text>
+        <Text>homgG -- Orders</Text>
       </View>
     );
   }
